8056e13baf34f4d546634b8cd3a7facd19a7085f
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2020-09-02 07:23:33 +0200

    black formatting

--- caas/__init__.py
+++ caas/__init__.py
@@ -9,6 +9,7 @@ def client(name: str) -> Callable[..., Callable[..., T]]:
     Use a service from the system. All the input and output data
     should be serializable.
     """
+
     def wrapper_builder(f: Callable[..., T]) -> Callable[..., T]:
         @functools.wraps(f)
         def wrapper(*args, **kw) -> T:
@@ -24,6 +25,7 @@ def service(name: str) -> Callable[..., Callable[..., T]]:
     Expose a service to the system. All the input and output data
     should be serializable.
     """
+
     def wrapper_builder(f: Callable[..., T]) -> Callable[..., T]:
         @functools.wraps(f)
         def wrapper(*args, **kw) -> T:
@@ -39,6 +41,7 @@ def intercept(name: str) -> Callable[..., Callable[..., T]]:
     Intercept service calls. All the inputs and output data should
     be serializable.
     """
+
     def wrapper_builder(f: Callable[..., T]) -> Callable[..., T]:
         @functools.wraps(f)
         def wrapper(*args, **kw) -> T:
