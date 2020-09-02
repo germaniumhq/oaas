33a1ea41406a8c1569d97721b8f342db621bcc8a
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2020-09-02 07:07:45 +0200

    Initial commit

---
+++ caas/__init__.py
@@ -0,0 +1,49 @@
+import functools
+from typing import Callable, TypeVar
+
+T = TypeVar("T")
+
+
+def client(name: str) -> Callable[..., Callable[..., T]]:
+    """
+    Use a service from the system. All the input and output data
+    should be serializable.
+    """
+    def wrapper_builder(f: Callable[..., T]) -> Callable[..., T]:
+        @functools.wraps(f)
+        def wrapper(*args, **kw) -> T:
+            return f(*args, **kw)
+
+        return wrapper
+
+    return wrapper_builder
+
+
+def service(name: str) -> Callable[..., Callable[..., T]]:
+    """
+    Expose a service to the system. All the input and output data
+    should be serializable.
+    """
+    def wrapper_builder(f: Callable[..., T]) -> Callable[..., T]:
+        @functools.wraps(f)
+        def wrapper(*args, **kw) -> T:
+            return f(*args, **kw)
+
+        return wrapper
+
+    return wrapper_builder
+
+
+def intercept(name: str) -> Callable[..., Callable[..., T]]:
+    """
+    Intercept service calls. All the inputs and output data should
+    be serializable.
+    """
+    def wrapper_builder(f: Callable[..., T]) -> Callable[..., T]:
+        @functools.wraps(f)
+        def wrapper(*args, **kw) -> T:
+            return f(*args, **kw)
+
+        return wrapper
+
+    return wrapper_builder
---
+++ test/test_registrations.py
@@ -0,0 +1,42 @@
+from typing import Any, List
+
+import caas
+
+
+@caas.client("users-get")
+def get_users(group: str) -> List[Any]: ...
+
+
+@caas.client("datastore")
+class DataStore:
+    def put_item(self, key: str, value: Any) -> None: ...
+
+    def get_item(self, key: str) -> None: ...
+
+    def remove_item(self, key: str) -> None: ...
+
+
+@caas.service("users-get")
+def get_user_list(group: str) -> List[Any]: ...
+
+
+@caas.service("datastore")
+class DataStoreService:
+    def put_item(self, key: str, value: Any) -> None: ...
+
+    def get_item(self, key: str) -> None: ...
+
+    def remove_item(self, key: str) -> None: ...
+
+
+@caas.service("intercept")
+def get_user_intercept(group: str) -> List[Any]: ...
+
+
+@caas.service("datastore")
+class DataStoreIntercept:
+    def put_item(self, key: str, value: Any) -> None: ...
+
+    def get_item(self, key: str) -> None: ...
+
+    def remove_item(self, key: str) -> None: ...
