de2119c42066020c2f4cf4cd640e56209607b2e7
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2020-09-08 23:49:00 +0200

    Black cleanup

--- oaas/server_provider.py
+++ oaas/server_provider.py
@@ -9,6 +9,7 @@ class ServerMiddleware(metaclass=abc.ABCMeta):
     Defines a serialization provider that can interpret the
     registrations and invoke the methods.
     """
+
     @abc.abstractmethod
     def serve(self) -> None:
         ...
